99f652a46586b9bfe789ffb8f01e0229c29deb56
val4oss <val4oss@pm.me>
2026-04-12 22:41:15 +0200

    snap only on some sections

--- src/config/sections.ts
+++ src/config/sections.ts
@@ -9,10 +9,11 @@ export const SECTION_IDS = {
 export type SectionId = typeof SECTION_IDS[keyof typeof SECTION_IDS];
 
 // Order used by SnapScroll
+// Only use with sections that are not full screen height.
 export const ORDERED_SECTIONS: SectionId[] = [
   SECTION_IDS.home,
   SECTION_IDS.about,
   SECTION_IDS.artists,
-  SECTION_IDS.gallery,
-  SECTION_IDS.contact,
+  //SECTION_IDS.gallery,
+  //SECTION_IDS.contact,
 ];
